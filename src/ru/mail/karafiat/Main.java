5179e85fa508b9755ab7e40bc5016c5647a560e1
Екатерина Антонова <karafiat@mail.ru>
2022-04-03 21:37:49 +0700

    add second output

--- src/ru/mail/karafiat/Main.java
+++ src/ru/mail/karafiat/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, mentor");
+        System.out.println("It's second message");
 	// write your code here
     }
 }
