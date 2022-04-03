0a465a9934d3f67df5258b654f7f654e93ec8d17
Екатерина Антонова <karafiat@mail.ru>
2022-04-03 21:43:08 +0700

    I changed the second output message AGAIN

--- src/ru/mail/karafiat/Main.java
+++ src/ru/mail/karafiat/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, mentor");
-        System.out.println("It's second message");
+        System.out.println("It's second message. I change it again");
 	// write your code here
     }
 }
